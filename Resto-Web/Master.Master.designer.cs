3391e870f5c68d6089c9c045ab6061b9a3bb419a
Lucas Gruden <lucas.gruden@alumnos.frgp.utn.edu.ar>
2023-06-15 17:33:18 -0300

    Se agregaron las paginas Login, Registrarse y Error.

--- Resto-Web/Master.Master.designer.cs
+++ Resto-Web/Master.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Resto_Web
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// lblCantElementosCarrito control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblCantElementosCarrito;
-
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
